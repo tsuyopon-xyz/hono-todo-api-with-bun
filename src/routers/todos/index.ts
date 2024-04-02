e36dc1e9f021322e5dcd8779dd3aa9c55cdef159
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2024-04-03 00:15:25 +0900

    add find route to routers/todos/index.ts

--- src/routers/todos/index.ts
+++ src/routers/todos/index.ts
@@ -1,11 +1,14 @@
 import { Hono } from 'hono';
 import { addRouteForCreateTodo } from './create';
 import { addRouteForGetTodos } from './list';
+import { addRouteForGetTodo } from './find';
 import TodoRepository from '@src/datasources/TodoRepository';
 import CreateTodoUseCase from '@src/usecases/CreateTodoUsecase';
 import CreateTodoController from '@src/controllers/todo/CreateTodoController';
 import GetTodosUsecase from '@src/usecases/GetTodosUsecase';
 import GetTodosController from '@src/controllers/todo/GetTodosController';
+import GetTodoUsecase from '@src/usecases/GetTodoUsecase';
+import GetTodoController from '@src/controllers/todo/GetTodoController';
 
 const todoRouter = new Hono();
 
@@ -13,10 +16,13 @@ const todoRouter = new Hono();
 const todoRepository = new TodoRepository();
 const createTodoUsecase = new CreateTodoUseCase(todoRepository);
 const createTodoController = new CreateTodoController(createTodoUsecase);
-const getTodoUsecase = new GetTodosUsecase(todoRepository);
-const getTodosController = new GetTodosController(getTodoUsecase);
+const getTodosUsecase = new GetTodosUsecase(todoRepository);
+const getTodosController = new GetTodosController(getTodosUsecase);
+const getTodoUsecase = new GetTodoUsecase(todoRepository);
+const getTodoController = new GetTodoController(getTodoUsecase);
 
 addRouteForCreateTodo({ todoRouter, createTodoController });
 addRouteForGetTodos({ todoRouter, getTodosController });
+addRouteForGetTodo({ todoRouter, getTodoController });
 
 export default todoRouter;
